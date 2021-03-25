e0e6d69b072c1e58e11e5d635cb484e2aeb989c5
Shubham Sachdeva <shubham.sachdeva0499@gmail.com>
2021-03-25 01:00:37 -0400

    first pass

--- PrecomputeRetargetLUT/PrecomputeRetargetLUT.cpp
+++ PrecomputeRetargetLUT/PrecomputeRetargetLUT.cpp
@@ -40,6 +40,54 @@ bool CheckBounds(int x, int max)
     return false;
 }
 
+
+float Energy(unsigned char* data, int dimension)
+{
+    float x;
+    float y;
+
+    GenerateR2Sequence(1, x, y);
+
+    int xOffset = static_cast<int>(x * dimension);
+    int yOffset = static_cast<int>(y * dimension);
+    
+    int bytesPerLine = dimension * 4;
+
+    float energy = 0.0f;
+    for (size_t j = 0; j < dimension; j++)
+    {
+        for (size_t i = 0; i < dimension; i++)
+        {
+            auto pixel = data + j * bytesPerLine + i * 4;
+
+            int newX = (i + xOffset) % dimension;
+            int newY = (j + yOffset) % dimension;
+
+            auto movedPixel = data + newY * bytesPerLine + newX * 4;
+
+            float distanceSquared = (movedPixel - pixel)* (movedPixel - pixel);
+            energy += distanceSquared;
+
+        }
+    }
+
+    return energy;
+}
+
+float map(float input, float input_start, float input_end, float output_start, float output_end)
+{
+    double slope = 1.0 * (output_end - output_start) / (input_end - input_start);
+    float output = output_start + slope * (input - input_start);
+
+    return output;
+}
+
+float PixelDistance(float ax, float ay, float bx, float by)
+{
+    float dist = ((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by));
+    return dist;
+}
+
 int main()
 {
     int width;
@@ -48,11 +96,14 @@ int main()
     unsigned char* data = stbi_load("Textures/BlueNoise512.png", &width, &height, &comp, 4);
 
     float initialTemperature = 1.f;
-    float minTemperature = 0.0001f;
-    float reductionFactor = 0.9f;
-    unsigned char* dataOut = new unsigned char[width * height * 4];
+    float reductionFactor = 0.0001f;
+    float swapCountFactor = 0.01f;
 
-    srand(time(NULL));
+    int64_t numSwaps = int64_t(swapCountFactor * double(512 * 512)); // how many swaps per trial
+    float coolingRate = 1.0f / (reductionFactor * double(32 * 32*32*32)); // how much the temperature cools each iteration
+
+    float* dataOut = new float[width * height * 4];
+    unsigned char* swapData = new unsigned char[width * height * 4];
 
     std::random_device rd;
 
@@ -62,99 +113,67 @@ int main()
     int bytesPerLine2 = width * 2;
 
     float T = initialTemperature;
-    //while (T > minTemperature)
-    //{
-    //    std::uniform_int_distribution<int> dist(-3, 3);
-    //    std::uniform_real_distribution<float> fDist(0.f, 1.f);
-    //    int offset = 0;
-    //    for (size_t j = 0; j < height; j++)
-    //    {
-    //        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-    //        for (size_t i = 0; i < width; i++)
-    //        {
-    //            int randomNumX = dist(random);
-    //            int randomNumY = dist(random);
-    //
-    //            int locX = i + randomNumX;
-    //            int locY = j + randomNumY;
-    //            if (CheckBounds(j, width) && CheckBounds(i, height))
-    //            {
-    //                auto pixel = data + j * bytesPerLine + i * 4;
-    //
-    //                float r2X, r2Y;
-    //
-    //                GenerateR2Sequence(1, r2X, r2Y);
-    //
-    //                auto ir2X = static_cast<int>(r2X * width);
-    //                auto ir2Y = static_cast<int>(r2Y * height);
-    //
-    //                auto modifiedPixel = data + ir2Y * bytesPerLine + ir2X * 4;
-    //
-    //                if (((pixel[0] - modifiedPixel[0]) / T) > fDist(random))
-    //                {
-    //                    unsigned char temp = pixel[0];
-    //                    pixel[0] = modifiedPixel[0];
-    //                    modifiedPixel[0] = pixel[0];
-    //                }
-    //            }
-    //
-    //            
-    //        }
-    //    }
-    //
-    //    T *= reductionFactor;
-    //}
 
+    float energy = Energy(data, 512);
 
-    std::uniform_int_distribution<int> dist(-3, 3);
-    std::uniform_real_distribution<float> fDist(0.f, 1.f);
-    int offset = 0;
-    for (size_t j = 0; j < height; j++)
+    std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+    std::uniform_int_distribution<int> iDist(0, 512);
+
+    for (size_t j = 0; j < 512; j++)
     {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (size_t i = 0; i < width; i++)
+        for (size_t i = 0; i < 512; i++)
         {
-    
-            float T = initialTemperature;
-            while (T > minTemperature)
-            {
-    
-                int randomNumX = dist(random);
-                int randomNumY = dist(random);
-                int locX = i + randomNumX;
-                int locY = j + randomNumY;
-                if (CheckBounds(locX, width) && CheckBounds(locY, height))
-                {
-                    auto pixel = data +  locY* bytesPerLine + locX * 4;
-                    auto outLoc = dataOut + j * bytesPerLine + i * 4;
-                    float r2X, r2Y;
-    
-                    GenerateR2Sequence(1, r2X, r2Y);
-    
-                    auto ir2X = static_cast<int>(r2X * width);
-                    auto ir2Y = static_cast<int>(r2Y * height);
-    
-                    auto modifiedPixel = data + ((j+ir2Y)%height) * bytesPerLine + ((i+ir2X)%width) * 4;
-    
-                    if (((pixel[0] - modifiedPixel[0]) / T) > fDist(random))
-                    {
-                        outLoc[0] = i + ir2X;
-                        outLoc[1] = j + ir2Y;
-                        outLoc[2] = 0;
-                        outLoc[3] = 255;
+            auto pixel = dataOut + j * bytesPerLine + i * 4;
 
-                    }
-                }
+            float xVal = map(i, 0, 511, 0, 255);
+            float yVal = map(j, 0, 511, 0, 255);
 
-                T *= reductionFactor;
-            }
+            pixel[0] = xVal;
+            pixel[1] = yVal;
+            pixel[2] = 0;
+            pixel[3] = 1.f;
         }
     }
 
 
-    stbi_write_png("Textures/Retarget.png", width, height, 4, dataOut, 0);
+    for (int64_t swapIndex = 0; swapIndex < numSwaps; ++swapIndex)
+    {
+
+        std::cerr << "\rSwaps Left " << numSwaps - swapIndex <<' ' <<std::flush;
+
+        T = std::max(T - coolingRate, 0.0f);
+
+        swapData = data;
+
+        uint32_t indexAx = iDist(random);
+        uint32_t indexAy = iDist(random);
+
+        uint32_t indexBx = iDist(random);
+        uint32_t indexBy = iDist(random);
+
+        auto indexA = indexAy * bytesPerLine + indexAx * 4;
+        auto indexB = indexBy * bytesPerLine + indexBx * 4;
+
+        std::swap(swapData[indexA], swapData[indexB]);
+
+        float swapEnergy = Energy(swapData, 512);
+
+        float rng01 = dist(random);
+        if (((swapEnergy < energy) && PixelDistance(indexAx, indexAy,indexBx, indexBy)<=36) || rng01 < T)
+        {
+            data = swapData;
+            energy = swapEnergy;
+
+            std::swap(dataOut[indexA], dataOut[indexB]);
+
+        }
+
+    }
+
+    stbi_write_png("Textures/Retarget1.png", width, height, 4, dataOut, 0);
 
     delete[] dataOut;
-
+    delete[] swapData;
 
 }
